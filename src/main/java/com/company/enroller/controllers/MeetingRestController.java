9b19140f3be30d76f9af7ad2e1dc391066862bb2
CodeWithN <ngmiterek@vp.pl>
2022-05-05 01:25:02 +0200

    Possibility to find single meeting

--- src/main/java/com/company/enroller/controllers/MeetingRestController.java
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -1,6 +1,7 @@
 package com.company.enroller.controllers;
 
 
+import com.company.enroller.model.Participant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +29,16 @@ public class MeetingRestController {
     public ResponseEntity<?> getMeetings() {
         Collection<Meeting> meetings = meetingService.getAll();
         return new ResponseEntity<Collection<Meeting>>(meetings, HttpStatus.OK);
+    }
+
+    //Find one meeting by ID
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getMeetingById(@PathVariable("id") long id) {
+        Meeting meeting = meetingService.findById(id);
+        if (meeting == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Meeting>(meeting, HttpStatus.OK);
     }
 
 
